6c1c4fb8db09f7b5cf2266c0926bc0d9fbf41cbf
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-04-04 18:32:46 +0100

    styling videodetail

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,40 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
-import youtube from '../apis/youtube';
+import VideoDetail from "./VideoDetail";
+import youtube from "../apis/youtube";
 
-const KEY = 'AIzaSyDWr-tpje_mR2m4hMC5KnaO75FvyfldYvg';
+const KEY = "AIzaSyDWr-tpje_mR2m4hMC5KnaO75FvyfldYvg";
 
 class App extends React.Component {
-	state = { videos: [] };
+	state = { videos: [], selectedVideo: null };
 
-  onTermSubmit = async (term) => {
-     const response = await youtube.get("/search", {
-      params: {
-        q: term,
+	onTermSubmit = async (term) => {
+		const response = await youtube.get("/search", {
+			params: {
+				q: term,
 				part: "snippet",
-				type: 'video',
-        maxResults: 5,
-        key: KEY
-      }
-		})
+				type: "video",
+				maxResults: 5,
+				key: KEY,
+			},
+		});
 		this.setState({ videos: response.data.items });
+	};
+
+	onVideoSelect = (video) => {
+		this.setState({selectedVideo: video});
 	};
 
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onTermSubmit={this.onTermSubmit} />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					videos={this.state.videos}
+					onVideoSelect={this.onVideoSelect}
+				/>
 			</div>
 		);
 	}
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import moment from 'moment';
-import './VideoItem.css';
+import React from "react";
+import moment from "moment";
+import "./VideoItem.css";
 
-const VideoItem = ({ video }) => {
-  const timeAgo = moment(video.snippet.publishedAt).fromNow();
+const VideoItem = ({ video, onVideoSelect }) => {
+	const timeAgo = moment(video.snippet.publishedAt).fromNow();
 
-  return (
-    <div className="item">
-      <img className="ui tiny image" key={video.id.videoId} src={video.snippet.thumbnails.medium.url} alt={video.snippet.description}/>
-      <div className="content">
-        <div className="header">{video.snippet.title}</div>
-        <div className="meta">
-          <span className="published">Published {timeAgo}</span>
-        </div>
-        <div className="description">{video.snippet.description}</div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div onClick={() => onVideoSelect(video)} className="item">
+			<img
+				className="ui tiny image"
+				key={video.id.videoId}
+				src={video.snippet.thumbnails.medium.url}
+				alt={video.snippet.description}
+			/>
+			<div className="content">
+				<div className="header">{video.snippet.title}</div>
+				<div className="meta">
+					<span className="published">Published {timeAgo}</span>
+				</div>
+				<div className="description">{video.snippet.description}</div>
+			</div>
+		</div>
+	);
+};
 
 export default VideoItem;
